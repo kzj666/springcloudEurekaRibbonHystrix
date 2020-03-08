66f8968108413fa0b757d56d7c95f36ad872f14c
kzj <2319680237@qq.com>
2020-03-08 14:27:03 +0800

    Initial commit

---
+++ springcloud-consumer-dept-80/src/main/java/com/common/springcloud/DeptConsumer_80.java
@@ -0,0 +1,19 @@
+package com.common.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+//服务降级
+//Ribbon 和 Eureka 整合以后,客户端可以直接调用, 不用关心ip地址和端口号
+@SpringBootApplication
+@EnableEurekaClient
+//@RibbonClient(name = "SPRINGCLOUD-PROVIDER-DEPT")//轮询
+@EnableCircuitBreaker   //添加对熔断的支持
+public class DeptConsumer_80 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_80.class, args);
+    }
+}
